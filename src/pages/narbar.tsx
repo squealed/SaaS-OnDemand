159d55bba5cbed6ed7990fe932364b1569c3c447
squealed <neilwalter7@gmail.com>
2024-11-02 05:27:51 -0400

    fix nav bar add balance query to navbar

--- src/pages/narbar.tsx
+++ src/pages/narbar.tsx
@@ -1,14 +1,15 @@
 import { signIn, signOut, useSession } from "next-auth/react"
-import Link from "next/link";
 import { api } from "~/utils/api";
-
+import { useState, useEffect } from "react";
 import { Palette } from "lucide-react";
+import Link from "next/link";
 
 export default function NavBar(){
     const { data: sessionData } = useSession();
-    if (sessionData){
-      const balance = api.credits.getCredits.useQuery();
-    }
+    
+    const { data: balanceData } = api.credits.getCredits.useQuery(undefined, {
+      enabled: !!sessionData, // Only run query if user is authenticated
+    });
     return (
         <nav className="fixed top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
           <div className="max-w-7xl mx-auto flex h-16 items-center justify-between px-4 md:px-8">
@@ -45,11 +46,16 @@ export default function NavBar(){
               >
               Generate Image
               </Link>}
+              {sessionData && balanceData !== null && (
+                <p className="text-sm font-medium transition-colors">
+                  Balance: ${balanceData}
+                </p>
+              )}
             </div>
     
             <div className="w-[200px] flex justify-end">
             <div className="flex flex-col gap-4">
-                                <button className="btn btn-secondary btn-block group !btn-block !btn-sm" onClick={sessionData ? () => void signOut() : () => void signIn()}> {sessionData ? "Sign out" : "Sign in"}<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-arrow-right"><path d="M5 12h14"/><path d="m12 5 7 7-7 7"/></svg></button>
+                                <button className="btn btn-secondary btn-block group !btn-sm" onClick={sessionData ? () => void signOut() : () => void signIn()}> {sessionData ? "Sign out" : "Sign in"}<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-arrow-right"><path d="M5 12h14"/><path d="m12 5 7 7-7 7"/></svg></button>
                             </div>
             </div>
           </div>
